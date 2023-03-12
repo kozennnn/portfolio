aaecf8a0163fd5f310b70c39a6cc77f526dd810d
Mathis HOUIS <mathishouis@gmail.com>
2023-03-12 04:08:47 +0100

    Created logo component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="app">
+    <logo/>
+  </div>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -9,24 +12,12 @@
 <style lang="scss">
 @import "@/sass/style.scss";
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.app {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  padding: 60px;
 }
 </style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="logo">
+    <div class="logo__text">./MATHIS</div><br/>
+    <div class="logo__text">HOUIS</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Logo',
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "@/sass/core/colors.scss";
+
+.logo {
+  display: flex;
+  flex-direction: column;
+  align-items: self-start;
+  margin-top: 9px;
+
+  &__text {
+    font-family: "Druk Wide Bold", sans-serif;
+    font-size: 40px;
+    color: $text-900;
+    line-height: 14px;
+    position: relative;
+
+    &:last-child {
+      position: relative;
+      display: inline-block;
+      &:after {
+        content:"";
+        width: 38px;
+        height: 30px;
+        right: -39px;
+        bottom: -8px;
+        background: $text-900;
+        animation: blink 1s infinite;
+        position: absolute;
+      }
+    }
+  }
+}
+
+@keyframes blink {
+  0% {
+    opacity: 0;
+  }
+
+  50% {
+    opacity: 0;
+  }
+
+  51% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
+</style>
